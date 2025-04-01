700a119c18fdc84057531c16881e093aba27bd3a
l1nxGD <useer095698@gmail.com>
2025-04-01 21:39:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 app = Flask('Programme lair')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 class Summary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,14 +22,6 @@ class Summary(db.Model):
 def main():
     repositories = Summary.query.all()
     return render_template('index.html', repository_list=repositories)
-
-
-@app.route('/in_stock/<product_id>', methods=['PATCH'])
-def modify_product(product_id):
-    resume = Summary.query.get(product_id)
-    resume.in_stock = request.json['in_stock']
-    db.session.commit()
-
 
 @app.route('/add', methods=['POST'])
 def add_product():
